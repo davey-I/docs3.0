4f85c4a372464894f3562a4539265d212ac15b52
davey-I <dave.indermuehle@gmail.com>
2025-05-14 11:19:28 +0200

    make content foldable and add foldable template to keybinding ctrl+shift+q

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-document.getElementById("hans")
+/* Function to fold/unfold the foldable containers */
+function toggleFoldableContent(ID){
+
+   content_classname = "foldable-content-"
+   conc_string_content = content_classname.concat(ID)
+
+   let foldContent = document.getElementById(conc_string_content)
+
+   if (foldContent.classList.contains('folded')) {
+    foldContent.classList.replace('folded', 'unfolded')
+   } else {
+    foldContent.classList.replace('unfolded', 'folded')
+   }
+}
